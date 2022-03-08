72a3928678e5219098cc2992aad4264a1923c4ce
Victor Pavelescu <victor.pavelescu@endava.com>
2022-03-08 15:57:16 +0200

    Fixed deploy to github pages actions

--- dentalapp-frontend/webpack/webpack.prod.js
+++ dentalapp-frontend/webpack/webpack.prod.js
@@ -2,24 +2,11 @@ const EventHooksPlugin = require('event-hooks-webpack-plugin');
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 const paths = require('../config/paths');
-const fn = require('../config/utils');
 
 module.exports = (envVars) =>
   merge(commonConfig, {
     mode: 'production',
-    plugins: [
-      new EventHooksPlugin({
-        beforeRun: () => {
-          const { device, phase } = envVars;
-          const deviceConfigPath = fn.getDeviceConfigPath(device, phase);
-
-          //Copying source files to build
-          fn.copyPublicFolderFiles();
-          //Copying device source files to build
-          fn.copyFolder(deviceConfigPath, paths.appBuild);
-        },
-      }),
-    ],
+    plugins: [new EventHooksPlugin({})],
     output: {
       path: paths.appBuild, // will be serving from this directory
       publicPath: paths.publicUrlOrPath,
