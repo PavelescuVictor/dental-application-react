ce7cc04ed88e99f9ac0a10096629af77e15e405f
Victor Pavelescu <victor.pavelescu@endava.com>
2022-03-10 16:14:24 +0200

    SplashScreen Fix

--- dentalapp-frontend/src/components/splashScreen/SplashScreen.tsx
+++ dentalapp-frontend/src/components/splashScreen/SplashScreen.tsx
@@ -1,19 +1,18 @@
 import { Image, Logo } from 'components';
-import { Background } from 'assets/images';
+import svgAssets from 'assets/images';
 import StyledSplashScreen from './SplashScreen.style';
 
-const SplashScreen = (): JSX.Element => {
-  console.log('splashScreen');
-  return (
-    <StyledSplashScreen>
-      <Image className="background" alt="Background" imageUrl={Background} />
-      <div className="overlay">
-        <div className="overlay__top" />
-        <div className="overlay__left" />
-      </div>
-      <Logo />
-    </StyledSplashScreen>
-  );
-};
+const { Background } = svgAssets;
+
+const SplashScreen = (): JSX.Element => (
+  <StyledSplashScreen>
+    <Image className="background" alt="Background" imageUrl={Background} />
+    <div className="overlay">
+      <div className="overlay__top" />
+      <div className="overlay__left" />
+    </div>
+    <Logo />
+  </StyledSplashScreen>
+);
 
 export default SplashScreen;
