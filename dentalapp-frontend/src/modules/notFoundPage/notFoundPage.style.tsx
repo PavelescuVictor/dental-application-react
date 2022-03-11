edc3f86d1ff14f00a66043cf9ac2cde25a877e35
Victor Pavelescu <victor.pavelescu@endava.com>
2022-03-11 16:10:23 +0200

    Added not found page fallback

--- dentalapp-frontend/src/modules/notFoundPage/notFoundPage.style.tsx
+++ dentalapp-frontend/src/modules/notFoundPage/notFoundPage.style.tsx
@@ -1,7 +1,34 @@
 import styled, { css } from 'styled-components';
 
 const StyledNotFoundPage = styled.div`
-  ${() => css``}
+  ${() => css`
+    min-height: 100vh;
+    position: relative;
+
+    .background {
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+      position: absolute;
+      z-index: -1;
+
+      svg {
+        width: 100%;
+      }
+    }
+
+    .not-found-page {
+      height: 100vh;
+      display: flex;
+      text-align: center;
+      align-items: center;
+      justify-content: center;
+
+      h1 {
+        font-size: 2rem;
+      }
+    }
+  `}
 `;
 
 StyledNotFoundPage.displayName = 'StyledNotFoundPage';
--- dentalapp-frontend/src/modules/notFoundPage/notFoundPage.tsx
+++ dentalapp-frontend/src/modules/notFoundPage/notFoundPage.tsx
@@ -1,13 +1,19 @@
-import { Page } from 'components';
-import { ViewType } from 'components/page/models';
+import { withAccessControl } from 'hocs';
+import { RouteAccessTypes } from 'routes/models';
+import svgAssets from 'assets/images';
 import StyledNotFoundPage from './notFoundPage.style';
+
+const { Background } = svgAssets;
 
 const NotFoundPage = (): JSX.Element => (
   <StyledNotFoundPage>
-    <Page viewType={ViewType.FULL_VIEWPORT}>
+    <div className="not-found-page">
+      <div className="background">
+        <Background />
+      </div>
       <h1>Page not found. Try another one</h1>
-    </Page>
+    </div>
   </StyledNotFoundPage>
 );
 
-export default NotFoundPage;
+export default withAccessControl(NotFoundPage, RouteAccessTypes.ALL_ACCESS);
